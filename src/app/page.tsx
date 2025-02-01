b4efef3373c582fc0115dc7f7810d4ca34aaf8ae
Bruno Aguiar <brunoaguiarw@gmail.com>
2025-02-01 15:48:06 -0300

    Mudança na tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   const [firstName, setFirstName] = useState<string | null>(null);
   const [userPoints, setUserPoints] = useState<number | null>(null);
   const [showPopup, setShowPopup] = useState(false);
-  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
